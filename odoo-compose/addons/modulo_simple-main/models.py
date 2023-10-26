1c6dff9c3484d8e454da14441c452635cc656f91
Alberto Saz <albertosaz@gmail.com>
2023-10-26 12:44:32 +0200

    Constrains edad

--- odoo-compose/addons/modulo_simple-main/models.py
+++ odoo-compose/addons/modulo_simple-main/models.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields, api
+from odoo import api, models, fields
 
 
 class Cliente(models.Model):
@@ -7,6 +7,18 @@ class Cliente(models.Model):
     email = fields.Char()
     phone = fields.Char()
     vendedor = fields.Many2one(comodel_name='salesianos.vendedor', string='vendedor') # relacion con el modelo vendedor comodel es el modelo con el que se relaciona, string es el nombre que se le da a la relacion
+    edad = fields.Integer()
+    # restricción de campos en el modelo @api.constraints añade un decorador (for record in self) cuando se crea un objeto, los datos se guardan en self -> record, en realidad no es un bucle, solo se ejecuta una vez; el registro que se está guardando en ese momento. El record es el objeto que se está guardando de la clase cliente, poniendo el .edad se accede al campo edad y si se quiere acceder a más campos hay que ponerlos en la lista, @api.constraints('edad', 'nombre', ...)
+    @api.constrains('edad', 'name') 
+    def _check_edad(self):
+        for record in self:
+            if record.edad < 18:
+                raise models.ValidationError('El cliente ' + record.name + ' debe ser mayor de edad')
+            elif record.edad > 100:
+                raise models.ValidationError('El cliente ' + record.name + ' no puede tener mas de 100 años')
+            else:
+                pass
+
     
 
 class Vendedor(models.Model):
